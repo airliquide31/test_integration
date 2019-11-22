153742c21339634ddf77846b4d9705bbde339201
francoisa <fr.autaa@laposte.net>
2019-11-22 15:32:27 +0100

    Simplify add more

--- bash_operator_test.py
+++ bash_operator_test.py
@@ -4,6 +4,8 @@ from datetime import timedelta
 import airflow
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
 from airflow.operators.dummy_operator import DummyOperator
 import pendulum
 
@@ -27,11 +29,24 @@ task = BashOperator(
 
 task2 = BashOperator(
     task_id='task2',
-    bash_command='echo "{{ task_instance_key_str }} Now {{ execution_date }} Prec {{ execution_date }}" && sleep 1',
+    bash_command='echo "{{ task_instance_key_str }} Now {{ execution_date }} Prec {{ execution_date }} {{ env }}" && sleep 1',
+    dag=dag,
+)
+
+
+def print_context(**context):
+    print(context)
+
+print_context = PythonOperator(
+    task_id="print_context",
+    python_callable=print_context,
+    provide_context=True,  # <====
     dag=dag,
 )
 
 task >> task2
+
+task2 >> print_context
 
 if __name__ == "__main__":
     dag.cli()
