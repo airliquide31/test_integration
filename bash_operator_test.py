7550d4b405fef00a25612fcf831de4892c79773f
francoisa <fr.autaa@laposte.net>
2019-11-22 16:15:23 +0100

    Simplify add more

--- bash_operator_test.py
+++ bash_operator_test.py
@@ -2,9 +2,10 @@ from builtins import range
 from datetime import timedelta
 
 import airflow
-from airflow.models import DAG
+from airflow.models import DAG , Variable
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+
 
 from airflow.operators.dummy_operator import DummyOperator
 import pendulum
@@ -23,30 +24,25 @@ dag = DAG(
 
 task = BashOperator(
     task_id='task1',
-    bash_command='echo "{{ task_instance_key_str }} Now {{ execution_date }}  Prev {{ prev_start_date_success }} " && sleep 1',
+    bash_command='echo "{{ task_instance_key_str }} Now {{ execution_date }}  Prev {{ var.value.last_execution_date_succes }} " && sleep 1',
     dag=dag,
     )
 
 task2 = BashOperator(
     task_id='task2',
-    bash_command='echo "{{ task_instance_key_str }} Now {{ execution_date }} Prec {{ execution_date }} {{ env }}" && sleep 1',
+    bash_command='echo "{{ task_instance_key_str }} Now {{ execution_date }} Prec {{ var.value.last_execution_date_succes }} " && sleep 1',
     dag=dag,
 )
 
 
-def print_context(**context):
-    print(context)
-
-print_context = PythonOperator(
-    task_id="print_context",
-    python_callable=print_context,
-    provide_context=True,  # <====
-    dag=dag,
+task3 = BashOperator(
+    task_id='SetDate',
+    bash_command='echo1',
+    baz = Variable.set("last_execution_date_succes", '{{ execution_date }}'),
+    dag=dag
 )
 
-task >> task2
-
-task2 >> print_context
+task >> task2 >> task3
 
 if __name__ == "__main__":
     dag.cli()
